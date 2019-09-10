dc3e08f8085c151ef3f90f9b647cd55a6c38ee72
Jacob Eddison <jacob.eddison@qa.com>
2019-09-10 14:54:34 +0100

    test for get single note added

---
+++ src/test/java/com/qa/controllers/NotesControllerTest.java
@@ -0,0 +1,71 @@
+package com.qa.controllers;
+
+import com.qa.models.Note;
+import com.qa.repository.NotesRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class NotesControllerTest {
+
+    @InjectMocks
+    private NotesController notesController;
+
+    @Mock
+    private NotesRepository repository;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetAllNotes(){
+        //create dummy database to access
+        List<Note> notesList = new ArrayList<>();
+        Note note = new Note();
+        note.setDescription("stuff");
+        note.setName("stuff");
+        notesList.add(note);
+
+        when(repository.findAll()).thenReturn(notesList);
+
+        assertEquals(notesController.listAllNotes().get(0).getName(),"stuff");
+
+    }
+    @Test
+    public void testGetNote(){
+        Note note = new Note();
+        note.setName("stuff");
+
+        when(repository.findOne(0L)).thenReturn(note);
+
+        assertEquals(notesController.getNote(0L).getName(),"stuff");
+
+    }
+    @Test
+    public void testCreateNote(){
+
+    }
+    @Test
+    public void testDeleteNote(){
+
+    }
+}
